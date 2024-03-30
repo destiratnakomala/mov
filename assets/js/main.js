ddeac8422c7ce2748b37e1429c446df3e9680d69
Desti Ratna Komala <124844924+destiratnakomala@users.noreply.github.com>
2024-03-30 20:25:59 +0700

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -55,3 +55,16 @@ sr.reveal('.home__data, .about__img, .skills__subtitle, .skills__text',{});
 sr.reveal('.home__img, .about__subtitle, .about__text, .skills__img',{delay: 400}); 
 sr.reveal('.home__social-icon',{ interval: 200}); 
 sr.reveal('.skills__data, .work__img, .contact__input',{interval: 200}); 
+
+
+/*===== certificates =====*/
+function scrollSlides(direction) {
+    const container = document.querySelector('.certificates__container');
+    const scrollAmount = 300; // Adjust this value to control scroll distance
+    if (direction === -1) {
+        container.scrollBy(-scrollAmount, 0);
+    } else if (direction === 1) {
+        container.scrollBy(scrollAmount, 0);
+    }
+}
+
